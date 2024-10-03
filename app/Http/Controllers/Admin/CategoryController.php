12301f5886cf758b93d15a6606e2d072f5c2a0f8
Asad Ullah <asadullah03189051077@gmail.com>
2024-10-04 00:03:51 +0500

    category done

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,9 +3,65 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(): View
+    {
+        $categories = Category::latest()->get();
+        return view('admin.category.list', compact('categories'));
+    }
+
+    public function create(): View
+    {
+        return view('admin.category.create');
+    }
+
+    public function edit($id): View
+    {
+        $category = Category::findOrFail($id);
+        return view('admin.category.update', compact('category'));
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'title' => 'required|unique:categories,title',
+            'slug' => 'required|unique:categories,slug',
+            'position' => 'required|boolean',
+        ]);
+        Category::create([
+            'title' => $request->title,
+            'slug' => $request->slug,
+            'position' => $request->position,
+        ]);
+
+        return to_route('admin.category.list')->with('success','Category successfully created');
+    }
+
+    public function update(Request $request, $id): RedirectResponse{
+        $request->validate([
+            'title' => 'required|unique:categories,title'.$id,
+            'slug' => 'required|unique:categories,slug',
+            'position' => 'required|boolean',
+        ]);
+        Category::findOrFail($id)->update([
+            'title' => $request->title,
+            'slug' => $request->slug,
+            'position' => $request->position,
+        ]);
+
+        return to_route('admin.category.list')->with('success','Category successfully updated');
+    }
+
+    public function destroy($id): RedirectResponse{
+        Category::destroy($id);
+        return to_route('admin.category.list')->with('success','Category successfully deleted');
+    }
+
 }
--- database/migrations/2024_10_03_182812_create_categories_table.php
+++ database/migrations/2024_10_03_182812_create_categories_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+    $table->string('title')->unique();
+    $table->string('slug')->unique();
+    $table->boolean('position')->default(0);
+    $table->timestamps();
         });
     }
 
