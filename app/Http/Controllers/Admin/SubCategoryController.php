7d485257d14cca90a34a55ab06a76b56e6b9f78c
Asad Ullah <asadullah03189051077@gmail.com>
2024-10-04 00:30:05 +0500

    sub ctegory done

---
+++ app/Http/Controllers/Admin/SubCategoryController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\SubCategory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class SubCategoryController extends Controller
+{
+    public function index() :View{
+        $subCategories = SubCategory::latest()->get();
+        return view('admin.subcategory.list', compact('subCategories'));
+    }
+
+    public function create() :View{
+        $categories = Category::latest()->get();
+        return view('admin.subcategory.create', compact('categories'));
+    }
+
+    public function store(Request $request) :RedirectResponse{
+        $request->validate([
+            'category_id' =>'required',
+            'title' =>'required|unique:sub_categories,title',
+            'slug' =>'required|unique:sub_categories,slug',
+            'position' =>'required|boolean'
+        ]);
+
+        SubCategory::create([
+            'category_id' => $request->category_id,
+            'title' => $request->title,
+            'slug'=>$request->slug,
+            'position'=>$request->position
+        ]);
+        return to_route('admin.subcategory.index')->with('success', 'Subcategory created successfully');
+    }
+
+    public function edit($id):View{
+
+        $subCategory = SubCategory::findOrFail($id);
+        return view('admin.subcategory.edit', compact('subCategory'));
+    }
+
+
+    public function destroy($id):RedirectResponse{
+        SubCategory::destroy($id);
+        return to_route('admin.subcategory.list')->with('success','Subcategory deleted successfully');
+    }
+
+    public function update(Request $request, $id):RedirectResponse{
+
+        $request->validate([
+            'category_id' =>'required',
+            'title' =>'required|unique:sub_categories,title',
+            'slug' =>'required|unique:sub_categories,slug',
+            'position' =>'required|boolean'
+        ]);
+
+        SubCategory::findOrFail($id)->update([
+            'category_id' => $request->category_id,
+            'title' => $request->title,
+            'slug' => $request->slug,
+            'position' => $request->position
+
+        ]);
+        return to_route('admin.subcategory.list')->with('success','Subcategory updated successfully');
+    }
+}
--- database/migrations/2024_10_03_182817_create_sub_categories_table.php
+++ database/migrations/2024_10_03_182817_create_sub_categories_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('sub_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->boolean('position')->default(0);
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
